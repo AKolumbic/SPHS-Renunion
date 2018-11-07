59f47bddfee32f1e93a832c27bacc965299931b6
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-11-07 10:21:51 -0800

    adds contact Model/Routers

---
+++ backend/ContactModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true}
+});
+
+const ContactModel = mongoose.model('Contact', ContactSchema);
+
+module.exports = ContactModel;
---
+++ backend/ContactRouter.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const Contact = require('./ContactModel.js');
+
+router
+    .route('/')
+    .get((req, res) => {
+        Contact
+            .find()
+            .then(response => {
+                res.status(200).json(response)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    errorMessage: "Contact Info could NOT be retrieved"
+                })
+            })
+    })
+    .post((req, res)=> {
+        const{ name, email } = req.body;
+        const ContactInfo = new Contact({ name, email });
+
+        ContactInfo
+            .save()
+            .then(response => {
+                res.status(201).json({
+                    success: "New Contact Information Added", response
+                })
+            })
+            .catch(err => {
+                res.status(500).json({
+                    errorMessage: "There was an error adding the Contact Info to the Database"
+                })
+            })
+    })
+
+module.exports = router;
