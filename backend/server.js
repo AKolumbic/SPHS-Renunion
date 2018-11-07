45225f0b528718d0b6325828c81a0e01f3b0e07c
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-11-07 11:02:58 -0800

    db connected in server

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
+// Variable Declarations
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const ContactInfo = require('./ContactRouter');
 const server = express();
 const port = process.env.PORT || 5555;
-const db = process.env.mongo;
+const db = process.env.MLAB;
 
+// Database configuration
+mongoose.Promise = global.Promise; // mongoose's promise library is deprecated, so we sub in the general ES6 promises here
+const databaseOptions = {
+    useNewUrlParser: true, // mongoose's URL parser is also deprecated, so we pass this in as a option to use the new one
+};
+mongoose.set('useCreateIndex', true); // collection.ensureIndex is also deprecated so we use 'useCreateIndex' instead
+
+// Connecting to Database
 mongoose
-    .connect(db) // mlab DB
+    .connect(db, databaseOptions) // mlab DB
     .then(response => {
         console.log("Connection Successful")
     })
@@ -16,12 +26,16 @@ mongoose
         console.log("Connection Failed")
     });
 
+// Server set-up
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/info', ContactInfo);
 
+// Sanity Check
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' });
 });
-  
+
+// Confirm the the API is running in Terminal.
 server.listen(port, () => console.log(`API running on port: ${port}`));
